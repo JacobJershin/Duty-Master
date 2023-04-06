212042691e48423b35a486876a9344aacadd2a2e
JacobJershin <pete231231@gmail.com>
2023-04-06 12:16:42 -0500

    update from local repo 4/6/23

---
+++ views/src/Home/index.js
@@ -0,0 +1,12 @@
+/*
+//Resolved
+const fetchPromise1 = fetch(url);
+    fetchPromise1.then(response => {
+      console.log(response);
+    });
+
+
+//Pending
+const fetchPromise = fetch(url);
+console.log(fetchPromise);
+*/
